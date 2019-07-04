e322649df7894c2452e1a6ac1f2bd90983692dcd
Fresh Legs <thecheapgeekz@gmail.com>
2019-07-03 20:18:51 -0400

    working on hashrouter, got submit to change number pulled from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import './App.css';
-//import {Route, HashRouter} from 'react-router-dom';
+import {Route, HashRouter, Link} from 'react-router-dom';
 import TriviaQ from './TriviaQ.js';
 import APICall from './APICall';
 
@@ -12,7 +12,8 @@ class App extends Component {
         this.state = {
             triviaMessage : 'test',
             factType : 'trivia',
-            randomNumber : 42
+            randomNumber : 42,
+            isFetching : true
         };
         this.triviaFacts = this.triviaFacts.bind(this);
         this.mathFacts = this.mathFacts.bind(this);
@@ -22,6 +23,7 @@ class App extends Component {
         this.numberSubmit = this.numberSubmit.bind(this);
     }
 
+    // 3 functions below change type of facts called from API
     triviaFacts = () => {
         this.setState({factType: 'trivia' })
         console.log(this.state.factType)
@@ -39,35 +41,42 @@ class App extends Component {
 
     numberSubmit = event => {
         event.preventDefault();
+        let newNumber = document.getElementById('user-submitted-number').value
+        this.setState({randomNumber: newNumber})
+        console.log(newNumber)
+        let displayNumber = <label> Will show fact for number: {newNumber} </label>
+        ReactDOM.render(displayNumber, document.getElementById('display-number'));
     }
 
-    // funciton to access API for facts repeatedly
+    // function to access API for facts repeatedly
     async callAPIFact() {
-    // generates random number 0-100 for API fetch
-    this.state.randomNumber = Math.floor(Math.random() * 101);
-    console.log(this.state.randomNumber)
-    let APISite = `https://numbersapi.p.rapidapi.com/${this.state.randomNumber}/${this.state.factType}?fragment=true&notfound=floor&json=true`
-    console.log(APISite)
-    let results = await fetch(APISite, {
-     headers: {
-      "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
-      "X-RapidAPI-Key": "9de36c8109msh4dc64975bcf7843p13ab52jsn921ab8e4043b"
-    }
-    })
-    // displays error in case fetch error from server
-    .catch((error) => {
-    console.log("Error " + error + " received from server!")
-   }) 
+        this.setState({isFetching: true})
+        // generates random number 0-100 for API fetch
+        // this.state.randomNumber = Math.floor(Math.random() * 101);
+        console.log(this.state.randomNumber)
+        let APISite = `https://numbersapi.p.rapidapi.com/${this.state.randomNumber}/${this.state.factType}?fragment=true&notfound=floor&json=true`
+        console.log(APISite)
+        let results = await fetch(APISite, {
+        headers: {
+        "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
+        "X-RapidAPI-Key": "9de36c8109msh4dc64975bcf7843p13ab52jsn921ab8e4043b"
+        }
+        })
+        // displays error in case fetch error from server
+        .catch((error) => {
+        console.log("Error " + error + " received from server!")
+    }) 
 
-   // parses response into JSON
-   let triviaFacts = await results.json()
-   // saves fact into App state
-   this.state.triviaMessage = triviaFacts.text
-   // renders trivia fact into DOM
-   let displayStatement = <p>{this.state.triviaMessage}</p>
-   ReactDOM.render(displayStatement, document.getElementById('populate-text'));
-   let displayNumber = <label> Current number showing facts for: {this.state.randomNumber} </label>
-   ReactDOM.render(displayNumber, document.getElementById('display-number'));
+    // parses response into JSON
+    let triviaFacts = await results.json()
+    this.setState({isFetching: false})
+    // saves fact into App state
+    this.state.triviaMessage = triviaFacts.text
+    // renders trivia fact into DOM
+    let displayStatement = <p>{this.state.triviaMessage}</p>
+    ReactDOM.render(displayStatement, document.getElementById('populate-text'));
+    let displayNumber = <label> Current number showing facts for: {this.state.randomNumber} </label>
+    ReactDOM.render(displayNumber, document.getElementById('display-number'));
     }
 
     
@@ -78,31 +87,42 @@ class App extends Component {
   
     
     render(){
+
+    if (this.state.isFetching) {
+        return (
+            <div id="populate-text">Loading...</div>
+        )
+    } else {
+        
         return (
             // display buttons, populate-text div
+            <div className="trivia-application">
             <div className="trivia-fact-selectors">
-                <button onClick={this.triviaFacts}>Numbers Trivia</button>
-                <button onClick={this.mathFacts}>Math Trivia</button>
-                <button onClick={this.yearFacts}>Year Trivia</button>
-                <div id="populate-text"></div>
-                <button onClick={this.callAPIFact}>New Fetch</button>
-                <form id="number-selector">
-                    <label>
-                        Enter the number you'd like to see random trivia for!
-                        <br></br>
-                        <input type = "text" name = "number-value" value = {this.numberSubmit} />
-                    </label>
-                    <input type = "submit" value = "Submit"/>
+                <HashRouter>
+                <Route exact path = "/" ><button onClick={this.triviaFacts}>Numbers Trivia</button></Route>
+                <Route path = "/math" ><button onClick={this.mathFacts}>Math Trivia</button></Route>
+                <Route path = "/year" ><button onClick={this.yearFacts}>Year Trivia</button></Route>
+                </HashRouter>
+            </div>
+            <div id="populate-text">
+            </div>
+            <button onClick={this.callAPIFact}>New Fetch</button>
+            <form id="number-selector">
+                <label>
+                    Enter the number you'd like to see random trivia for!
                     <br></br>
-                    <div id = "display-number"></div>
-                    {/* <label>
-                        {this.state.randomNumber}
-                    </label> */}
-                </form>
+                    <input id = "user-submitted-number" type = "text" name = "number-value"/>
+                </label>
+                <input type = "submit" value = "Submit" onClick={this.numberSubmit}/>
+                <div id = "display-number"></div>
+                {/* <label>
+                    {this.state.randomNumber}
+                </label> */}
+            </form>
             </div>
              
         );
-    }
+    }}
 }
 
 export default App;
--- src/TriviaQ.js
+++ src/TriviaQ.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 
-const TriviaQ = props => (
-    <p>{props.TriviaMessage}</p>
-)
+// var Triviaq = async callAPIFact => {
+//     // generates random number 0-100 for API fetch
+//     this.state.randomNumber = Math.floor(Math.random() * 101);
+//     console.log(this.state.randomNumber)
+//     let APISite = `https://numbersapi.p.rapidapi.com/${this.state.randomNumber}/${this.state.factType}?fragment=true&notfound=floor&json=true`
+//     console.log(APISite)
+//     let results = await fetch(APISite, {
+//      headers: {
+//       "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
+//       "X-RapidAPI-Key": "9de36c8109msh4dc64975bcf7843p13ab52jsn921ab8e4043b"
+//     }
+//     })
+//     // displays error in case fetch error from server
+//     .catch((error) => {
+//     console.log("Error " + error + " received from server!")
+//    }) 
+// }
 
-// return (TriviaQ);
 
-export default TriviaQ;
+// export default TriviaQ;
