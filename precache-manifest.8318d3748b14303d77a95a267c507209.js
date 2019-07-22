2d5ea5af5289d12cdb48adafdff41b9f5d10b8d9
Fresh Legs <thecheapgeekz@gmail.com>
2019-07-22 19:16:17 -0400

    Updates

---
+++ precache-manifest.8318d3748b14303d77a95a267c507209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d34c61075ffec8a3fdb4fe9d2ab1cf5",
+    "url": "/trivia-app/index.html"
+  },
+  {
+    "revision": "c6ddd04d563ac0163c3e",
+    "url": "/trivia-app/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "f027b1ba1f6d7f261ecc",
+    "url": "/trivia-app/static/css/main.662f7246.chunk.css"
+  },
+  {
+    "revision": "c6ddd04d563ac0163c3e",
+    "url": "/trivia-app/static/js/2.2c371703.chunk.js"
+  },
+  {
+    "revision": "f027b1ba1f6d7f261ecc",
+    "url": "/trivia-app/static/js/main.7fdc8b08.chunk.js"
+  },
+  {
+    "revision": "3b6865877693a7ab1de9",
+    "url": "/trivia-app/static/js/runtime~main.2396b5ef.js"
+  }
+]);
